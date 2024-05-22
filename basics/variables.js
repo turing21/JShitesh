b617f84add8c8c5f803456d7d8742b79bfad1f71
turing21 <71026760+turing21@users.noreply.github.com>
2024-05-22 08:42:11 +0000

    variables and datatypes

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountID= 12345
+let email="freg@gmail.com"
+var pw="123445"
+accountCity="kanpur"
+let a
+const b=0
+c=2
+
+// console.log("3+")
+console.log(pw)
+console.table([accountID, email, pw, accountCity,a,b,c])
